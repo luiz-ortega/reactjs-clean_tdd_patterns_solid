0a46d9edf1f3349fa431f17b49eeb8df859c1169
luiz-ortega <luizngust@hotmail.com>
2021-03-20 14:15:30 -0300

    chore: add react configs

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,13 @@
 module.exports = {
     roots: ['<rootDir>/src'],
-    collectCoverageFrom: [
-        '<rootDir>/src/**/*.{ts,tsx}',
-        '!<rootDir>/src/main/**/*',
-        '!<rootDir>/src/**/index.ts',
-        '!**/*.d.ts',
-    ],
+    collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
     coverageDirectory: 'coverage',
-    testPathIgnorePatterns: [
-        '<rootDir>/node_modules/',
-        '<rootDir>/src/main/test/cypress',
-    ],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/'],
     testEnvironment: 'jsdom',
     transform: {
         '.+\\.(ts|tsx)$': 'ts-jest',
     },
     moduleNameMapper: {
         '@/(.*)': '<rootDir>/src/$1',
-        '\\.scss$': 'identity-obj-proxy',
     },
 }
