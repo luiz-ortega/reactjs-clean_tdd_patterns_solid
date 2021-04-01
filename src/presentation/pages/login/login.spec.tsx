d5a7f3ecef2f241a416e03a8906cb7cf3c6bc22e
luiz-ortega <luizngust@hotmail.com>
2021-03-31 21:16:07 -0300

    refactor: change Validation interface

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -16,10 +16,12 @@ type SutTypes = {
 
 class ValidationSpy implements Validation {
     errorMessage: string
-    input: object
+    fieldName: string
+    fieldValue: string
 
-    validate(input: object): string {
-        this.input = input
+    validate(fieldName: string, fieldValue: string): string {
+        this.fieldName = fieldName
+        this.fieldValue = fieldValue
         return this.errorMessage
     }
 }
@@ -62,9 +64,8 @@ describe('Login Component', () => {
 
         const input = sut.getByTestId('email')
         fireEvent.change(input, { target: { value: 'any_email' } })
-        expect(validationSpy.input).toEqual({
-            email: 'any_email',
-        })
+        expect(validationSpy.fieldName).toBe('email')
+        expect(validationSpy.fieldValue).toBe('any_email')
     })
 
     test('Should call Validation with correct password', () => {
@@ -72,8 +73,7 @@ describe('Login Component', () => {
 
         const input = sut.getByTestId('password')
         fireEvent.change(input, { target: { value: 'any_password' } })
-        expect(validationSpy.input).toEqual({
-            password: 'any_password',
-        })
+        expect(validationSpy.fieldName).toBe('password')
+        expect(validationSpy.fieldValue).toBe('any_password')
     })
 })
