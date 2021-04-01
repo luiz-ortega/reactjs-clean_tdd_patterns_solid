ea599cd0e032560a46c69d173c7170455783116f
luiz-ortega <luizngust@hotmail.com>
2021-03-31 21:51:18 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -71,4 +71,15 @@ describe('Login Component', () => {
         expect(passwordStatus.title).toBe(validationStub.errorMessage)
         expect(passwordStatus.textContent).toBe('🔴')
     })
+
+    test('Should show valid password state if Validation succeeds', () => {
+        const { sut, validationStub } = makeSut()
+        validationStub.errorMessage = null
+        const input = sut.getByTestId('password')
+        fireEvent.input(input, { target: { value: faker.internet.password() } })
+
+        const passwordStatus = sut.getByTestId('password-status')
+        expect(passwordStatus.title).toBe('Tudo certo')
+        expect(passwordStatus.textContent).toBe('🟢')
+    })
 })
