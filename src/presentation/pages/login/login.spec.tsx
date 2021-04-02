c702c0cb8e17c19501a9938b6fc8c0bef8aaa482
luiz-ortega <luizngust@hotmail.com>
2021-04-01 21:51:58 -0300

    test: ensure login present spinner on submit

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -116,4 +116,22 @@ describe('Login Component', () => {
         ) as HTMLButtonElement
         expect(submitButton.disabled).toBe(false)
     })
+
+    test('Should show spinner on submit', () => {
+        const { sut } = makeSut()
+
+        const emailInput = sut.getByTestId('email')
+        fireEvent.input(emailInput, {
+            target: { value: faker.internet.email() },
+        })
+        const passwordInput = sut.getByTestId('password')
+        fireEvent.input(passwordInput, {
+            target: { value: faker.internet.password() },
+        })
+
+        const submitButton = sut.getByTestId('submitButtom')
+        fireEvent.click(submitButton)
+        const spinner = sut.getByTestId('spinner')
+        expect(spinner).toBeTruthy()
+    })
 })
