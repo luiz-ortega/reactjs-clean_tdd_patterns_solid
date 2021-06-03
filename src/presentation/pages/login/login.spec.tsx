804ec66180341ce61b36c85104ac2ecc2d40aa8f
luiz-ortega <luizngust@hotmail.com>
2021-06-03 17:56:56 -0300

    test: ensure Login redirects to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -23,7 +23,7 @@ type SutParams = {
     validationError: string
 }
 
-const history = createMemoryHistory()
+const history = createMemoryHistory({ initialEntries: ['/login'] })
 
 const makeSut = (params?: SutParams): SutTypes => {
     const validationStub = new ValidationStub()
