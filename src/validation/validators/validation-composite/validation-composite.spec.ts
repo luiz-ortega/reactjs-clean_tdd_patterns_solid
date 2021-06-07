4ff23106dd86eb95636f14aca193e91584b53439
luiz-ortega <luizngust@hotmail.com>
2021-06-07 19:12:31 -0300

    refactor: move validation test folder to correct place

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,5 +1,5 @@
-import { ValidationComposite } from './validation-composite'
-import { FieldValidationSpy } from '../test/mock-field-validation'
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
+import { FieldValidationSpy } from '@/validation/test/mock-field-validation'
 import faker from 'faker'
 
 type SubTypes = {
